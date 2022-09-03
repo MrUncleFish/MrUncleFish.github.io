c5b9a725ab997f39622fe4482fb755698a5c4e30
alexaos <alex2000mn@gmail.com>
2022-09-04 00:59:43 +0500

    next level of website

--- src/functions/mobile.ts
+++ src/functions/mobile.ts
@@ -1,5 +1,7 @@
 export const isMobile = () => window.innerHeight >= window.innerWidth;
 
+export const isTooWideForStats = () => (window.innerHeight * 0.95) < (window.innerWidth * 0.55);
+
 export const getMaxrixNumbersCount = () => {
 
     const oneLineSize = window.innerHeight / (100 / 1.8);
--- src/pages/StatsScreen/StatsScreen.tsx
+++ src/pages/StatsScreen/StatsScreen.tsx
@@ -1,14 +1,16 @@
 import './StatsScreen.scss'
-import React, {useEffect, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import MovingObjectContainer from "../../components/MovingObjectContainer/MovingObjectContainer";
 import heroAva from "../../assets/img/hero_ava.png";
+import {isMobile, isTooWideForStats} from "../../functions/mobile";
+import {MOVING_OBJECT_LIST} from "../../config/MovingObjectList";
 
 
 interface StatsScreenProps {
     isActive: boolean;
 }
 
-function StatsScreen({isActive} : StatsScreenProps) {
+function StatsScreen({isActive}: StatsScreenProps) {
 
     const [activeAnim, setActiveAnim] = useState(false);
     const [isUnlimited, setIsUnlimited] = useState(false);
@@ -36,28 +38,58 @@ function StatsScreen({isActive} : StatsScreenProps) {
 
     }, [changeChangeBlocked, currentChange, activeAnim, isUnlimited]);
 
+    if (isMobile()) return (
+        <div className="StatsScreen">
+            <div className="StatsContainer StatsContainer__mobile">
+                <div className="HeroContainer">
+                    <img src={heroAva} className="HeroAva"/>
+                    <div className="HeroNick">@KeyLord</div>
+                    <div>
+                        <div className="HeroDesc">
+                            Эта страница - кладезь некоторых моих увлечений/умений/etc!
+                        </div>
+                    </div>
+                </div>
+                <div className="GalleryContainer">
+                    {MOVING_OBJECT_LIST.map((item) => <div className="GalleryItem">
+                        <div className="FloatContainer_block__title">{item.title}</div>
+                        <div className="FloatContainer_block__desc">{item.desc}</div>
+                    </div>)}
+                </div>
+                <div className="GalleryCounter">
+                    1/3
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <div className="StatsScreen">
-            <div className="StatsContainer">
+            <div className={isTooWideForStats() ? "StatsContainer StatsContainer__wide" : "StatsContainer"}>
                 <MovingObjectContainer isActive={activeAnim}/>
                 <div className="HeroContainer">
                     <img src={heroAva} className="HeroAva"/>
                     <div className="HeroNick">@KeyLord</div>
 
                     <div className="HeroDesc">
-                        Эта страница - кладезь некоторых моих увлечений/умений/etc! Вы можете включить анимацию передвижения этих карточек, нажав на кнопку снизу, но будьте внимательны, заряд может истощиться!
-                        <span onClick={() => setIsUnlimited(true)} style={{cursor: 'pointer'}}>(нажмите на это предложение, чтобы отключить заряд)</span> Также вы можете навести курсор на карточку и остановить её.
+                        Эта страница - кладезь некоторых моих увлечений/умений/etc! Вы можете включить анимацию
+                        передвижения этих карточек, нажав на кнопку снизу, но будьте внимательны, заряд может
+                        истощиться!
+                        <span onClick={() => setIsUnlimited(true)} style={{cursor: 'pointer'}}>(нажмите на это предложение, чтобы отключить заряд)</span> Также
+                        вы можете навести курсор на карточку и остановить её.
                     </div>
 
                     <div className="HeroChangeName">Заряд анимации</div>
 
-                    <div className="battery"><div className="battery__charge" style={{left: currentChange - 100 + '%'}}/></div>
+                    <div className="battery">
+                        <div className="battery__charge" style={{left: currentChange - 100 + '%'}}/>
+                    </div>
 
                     {activeAnim
-                        ? <div onClick={() => setActiveAnim(false)} className="activate_button deactivated">отключить</div>
-                        : <div onClick={() => (currentChange > 0 || isUnlimited) && setActiveAnim(true)} className="activate_button">активировать</div>}
-
-                    <div className="anim_square"/>
+                        ? <div onClick={() => setActiveAnim(false)}
+                               className="activate_button deactivated">отключить</div>
+                        : <div onClick={() => (currentChange > 0 || isUnlimited) && setActiveAnim(true)}
+                               className="activate_button">активировать</div>}
 
                     {isUnlimited && <div className="secret_desc">// бесконечный заряд</div>}
                 </div>
